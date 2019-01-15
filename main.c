c46c03a52784cd9770fc78426de044ad9508fb8a
flagsa <flagsa@qq.com>
2019-01-15 10:50:43 +0800

    构建基本框架

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int qian=0;
+    printf("进入钱包管理系统\n");
+    while(1)
+    {
+        printf("1---存钱\n");
+        printf("2---取钱\n");
+        printf("3---余额\n");
+        printf("4---退出\n");
+        printf("请选择相应功能\n");
+        int code;
+        scanf("%d",&code);
+    }
+}
